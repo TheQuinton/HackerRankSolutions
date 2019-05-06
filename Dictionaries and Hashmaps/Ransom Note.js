0c64b8a4da162b5ee8a72b7dd0969a7a05a74141
Quinton <qschafer14@gmail.com>
2019-05-06 16:41:57 -0400

    Ransom Note Solution
    Link: https://www.hackerrank.com/challenges/ctci-ransom-note/problem
    Actual Code: checkMagazine function

---
+++ Dictionaries and Hashmaps/Ransom Note.js
@@ -0,0 +1,53 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the checkMagazine function below.
+function checkMagazine(magazine, note) {
+    let match = [],
+        success = false;
+    match = note.filter(words => {
+        if (magazine.includes(words)) {
+            magazine.splice(magazine.indexOf(words), 1);
+            return words;
+        }
+    });
+    if (match.length === note.length) {
+        success = true;
+    }
+    console.log(success ? "Yes" : "No");
+}
+
+function main() {
+    const mn = readLine().split(' ');
+
+    const m = parseInt(mn[0], 10);
+
+    const n = parseInt(mn[1], 10);
+
+    const magazine = readLine().split(' ');
+
+    const note = readLine().split(' ');
+
+    checkMagazine(magazine, note);
+}
